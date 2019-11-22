e117892bf67c43033fd5186c90936dd1a4b9a839
Brady Hill <bradyhill86@yahoo.com>
2019-11-22 13:45:08 -0600

    Added LogOut button and a UI element on Main Menu Scene to show current User

---
+++ Assets/Scripts/LogOut.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogOut : MonoBehaviour
+{
+    public GameObject canvas;
+    public void ClickLogOut()
+    {
+        canvas.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/LoginUIInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginUIInfo : MonoBehaviour
+{
+    public Text loginUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        loginUI.text = "Logged in as " + Main.Instance.newUser.GetName();
+    }
+}
